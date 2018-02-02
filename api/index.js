a1ca16780c4df58d3c39902182139d30f69d445d
shannonphu <shannonphu@sbcglobal.net>
2018-02-02 14:58:15 -0800

    added router and controller for user paths

--- api/index.js
+++ api/index.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const express = require('express');
 const http = require('http');
 const app = express();
@@ -5,14 +7,19 @@ const server = http.createServer(app);
 
 const config = require('./config');
 
-// Connect to database and declare database Models
-const db = require('./db');
-const User = db.User;
+// Connect to database
+const db = require('./models');
+
+// Get routers
+const userRouter = require('./routes/user');
 
 // Endpoints
 app.get('/', (req, res) => {
 	res.send("test endpoint");
 });
+
+// Set up router endpoints
+app.use("/user", userRouter);
 
 server.listen(config.server.port, () => {
 	console.log("Listening on port " + config.server.port);
