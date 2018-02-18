952efdcad6da0800a6cdd399e0ca77a20d694aeb
galenw <galenw@galenws-MacBook-Pro.local>
2018-02-17 19:01:05 -0800

    User crud implementation

--- api/models/User.js
+++ api/models/User.js
@@ -4,7 +4,11 @@ const mongoose = require('mongoose');
 
 // User Schema Definition
 var userSchema = new mongoose.Schema({
-	name: String
+	name: {type : String, required : true},
+	isAdmin: Boolean,
+	isMentor: Boolean,
+	skills: [String]	,
+	googleID: { type : String , unique : true, required : true }
 });
 
 // User Schema Methods
@@ -18,9 +22,14 @@ var userSchema = new mongoose.Schema({
  *     .then(user => console.log(user))
  *     .catch(error => console.error(error));
  */
-userSchema.statics.create = function(name) {
+
+userSchema.statics.create = function(name, GoogleID) {
 	var user = new this({
-		name: name
+		name: name,
+		isAdmin: false,
+		isMentor: false,
+		skills:[],
+		googleID: GoogleID
 	});
 
 	return new Promise((resolve, reject) => {
@@ -31,4 +40,100 @@ userSchema.statics.create = function(name) {
 	});
 };
 
+userSchema.statics.read = function(GoogleID)
+{
+	return new Promise((resolve , reject) => {
+		mongoose.model('User', userSchema).findOne({googleID: GoogleID}, (err,user)=>{
+			if(err) reject(err);
+			else resolve(user);
+		});
+	});
+}
+
+userSchema.statics.delete = function(GoogleID)
+{
+	return new Promise((resolve, reject)=>{
+		mongoose.model('User', userSchema).findOne({googleID: GoogleID}).remove((err,offer)=>{
+			if(err) reject(err);
+		});
+	});
+}
+
+
+// Static: User.doSomething
+// Instance method: 
+// var user;
+// user.doSomething()
+
+
+userSchema.methods.setisAdminTo = function(becomeAdmin)
+{
+	this.isAdmin = becomeAdmin;
+	console.log('set user name:' + this.name + ' isAdmin to ' + becomeAdmin);
+	
+	return new Promise((resolve, reject)=>{
+		this.save((error, changedUser) => {
+			if(error) reject(error);
+			else resolve(changedUser);
+			});
+		});
+}
+
+
+userSchema.methods.setisMentorTo = function(becomeMentor)
+{
+	this.isMentor = becomeMentor;
+	console.log('set user name: '+this.name+' isMentor to '+becomeMentor);
+
+	return new Promise((resolve,reject)=>{
+		this.save((err, changedUser)=>{
+			if(err) reject(err);
+			else resolve(changedUser);
+		});
+	});
+}
+
+userSchema.methods.addSkill = function(skill)
+{
+	//check whether skill exists, should i throw err, if yes how?
+	var exists = false;
+	for(var i = 0; i < this.skills.length; i++)
+	{	
+		if(this.skills[i]===skill){
+			exists = true;
+			break;
+		}
+	}
+	if(!exists){
+		this.skills.push(skill);
+		console.log('skill: '+skill+' added')
+	}
+
+	return new Promise((resolve, reject)=>{
+		this.save((err,skillfulUser)=>{
+			if(err) reject(err);
+			else resolve(skillfulUser);
+		});
+	});
+}
+
+userSchema.methods.removeSkill = function(skill)
+{
+	//assume the skill does exist
+	this.skills.pop(skill);
+	console.log('skill: '+skill+' removed')
+	return new Promise((resolve,reject)=>{
+		this.save((err,notSkillfulUser)=>{
+			if(err) reject(err);
+			else resolve(notSkillfulUser);
+		});
+	});
+}
+
+
 module.exports = mongoose.model('User', userSchema);
+
+
+
+
+
