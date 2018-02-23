fe0013c551d7378cc746a8d02c60c886c0f6950d
shannonphu <shannonphu@sbcglobal.net>
2018-02-22 21:03:28 -0800

    remove redux calls in Container for testing

--- client/src/components/Container.jsx
+++ client/src/components/Container.jsx
@@ -1,14 +1,10 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
 import Nav from './Nav';
-import * as textAction from '../actions/text.js';
-import { connect } from 'react-redux';
 
 class Container extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {};
-		this.props.addText('some text');
 	}
 
 	render() {
@@ -20,14 +16,4 @@ class Container extends Component {
 	}
 }
 
-Container.propTypes = {
-	addText: PropTypes.func
-};
-
-const mapDispatchToProps = (dispatch) => {
-	return {
-		addText: (textToAdd) => dispatch(textAction.addText(textToAdd))
-	};
-};
-
-export default connect(null, mapDispatchToProps)(Container);
+export default Container;
