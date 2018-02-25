d97d7f9206414dfb457eb6206aaf6daab44cff0c
Galen Wong <33505320+GalenWong@users.noreply.github.com>
2018-02-24 21:30:22 -0800

    Revert "Implemented User Mongoose CRUD methods"

--- api/models/User.js
+++ api/models/User.js
@@ -2,15 +2,9 @@
 
 const mongoose = require('mongoose');
 
-var User;
-
 // User Schema Definition
 var userSchema = new mongoose.Schema({
-	name: {type: String , required: true},
-	isAdmin: {type: Boolean , default: false},
-	isMentor: {type: Boolean , default: false},
-	skills: [String],
-	googleId: {type: String , unique: true, required: true}
+	name: String
 });
 
 // User Schema Methods
@@ -18,19 +12,15 @@ var userSchema = new mongoose.Schema({
 /**
  * Creates and saves a User object in the database
  * @param {string} user's name
- * @param {string} user's GoogleID (as hash)
  * @returns {User} newly saved User object
  * @example
- * User.create('Joe Bruin', 'someGoogleIdHash')
+ * User.create("Joe Bruin")
  *     .then(user => console.log(user))
  *     .catch(error => console.error(error));
  */
-
-userSchema.statics.create = function(name, googleId) {
+userSchema.statics.create = function(name) {
 	var user = new this({
-		name: name,
-		skills: [],
-		googleId: googleId
+		name: name
 	});
 
 	return new Promise((resolve, reject) => {
@@ -41,145 +31,4 @@ userSchema.statics.create = function(name, googleId) {
 	});
 };
 
-/**
- * Read and Retrieve a User object from the database
- * @param {string} user's googleID (as hash)
- * @returns {User} one User object with matching ID
- * @example
- * User.read('someGoogleIdHash')
- *     .then(user => console.log(user))
- *     .catch(error => console.error(error));
- */
-
-userSchema.statics.read = function(googleId) {
-	return new Promise((resolve , reject) => {
-		User.findOne({googleId: googleId}, (err, user) => {
-			if (err) reject(err);
-			else resolve(user);
-		});
-	});
-};
-
-/**
- * Delete a User object from the database
- * @param {string} user's googleID (as hash)
- * @returns {void} nothing
- * @example
- * User.delete('someGoogleIdHash')
- *     .catch(error => console.error(error));
- */
-
-userSchema.statics.delete = function(googleId) {
-	return new Promise((resolve, reject) => {
-		User.findOne({googleId: googleId}).remove((err, offer) => {
-			if(err) reject(err);
-			else resolve(offer);
-		});
-	});
-};
-
-/**
- * Change User's isAdmin status
- * @param {Boolean} User's new isAdmin status
- * @returns {User} changed user object
- * @example
- * User.read('someGoogleIdHash')
- *     .then(user =>  return user.setAdminStatus(true))
- *	   .catch(error => console.error(error));
- */
-
-userSchema.methods.setAdminStatus = function(adminStatus) {
-	this.isAdmin = adminStatus;
-	
-	return new Promise((resolve, reject) => {
-		this.save((error, user) => {
-			if(error) reject(error);
-			else resolve(user);
-		});
-	});
-};
-
-/**
- * Change User's isMentor status
- * @param {Boolean} User's new isMentor status
- * @returns {User} changed user object
- * @example
- * User.read('someGoogleIdHash')
- *     .then(user => return user.setMentorStatus(true))
- *	   .catch(error => console.error(error));
- */
-
-userSchema.methods.setMentorStatus = function(mentorStatus) {
-	this.isMentor = mentorStatus;
-
-	return new Promise((resolve, reject) => {
-		this.save((err, user) => {
-			if(err) reject(err);
-			else resolve(user);
-		});
-	});
-};
-
-/**
- * Add a new skill to the User object
- * If attempt to add a skill that already exists, 
- * an error will be thrown
- * @param {string} User's new skill
- * @returns {User} changed user object
- * @example
- * User.read('someGoogleIdHash')
- *     .then(user => return user.addSkill('python'))
- *	   .catch(error => console.error(error));
- */
-
-userSchema.methods.addSkill = function(skill) {
-	// skill does not exist if index is -1;
-	// skill exists already if index is >=0;
-	const index = this.skills.indexOf(skill);
-
-	if (index >= 0){
-		return new Promise(new Error('skill: '+ skill +' already exists!'));
-	}
-
-	this.skills.push(skill);
-
-	return new Promise((resolve, reject) => {
-		this.save((err, user) => {
-			if (err) reject(err);
-			else resolve(user);
-		});
-	});
-};
-
-/**
- * Remove a skill from the User object
- * If attempt to remove a skill that does not exists, 
- * an error will be thrown
- * @param {string} skill to remove from the User object
- * @returns {User} changed user object
- * @example
- * User.read('someGoogleIdHash')
- *     .then(user => return user.removeSkill('python'))
- *	   .catch(error => console.error(error));
- */
-
-userSchema.methods.removeSkill = function(skill) {
-	const index = this.skills.indexOf(skill);
-
-	if (index < 0){
-		return new Promise(new Error('skill: '+ skill +' does not exists!'));
-	}
-
-	this.skills.splice(index, 1);
-
-	return new Promise((resolve, reject) => {
-		this.save((err, user) => {
-			if (err) reject(err);
-			else resolve(user);
-		});
-	});
-};
-
-User = mongoose.model('User', userSchema);
-
-module.exports = User;
+module.exports = mongoose.model('User', userSchema);
