2a01e6ae5001b23d7a7f3e5d0470b9c68ecae5ce
Connie <connie90503@gmail.com>
2018-03-06 16:59:09 -0800

    Created a button

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,24 +1,26 @@
-import React, { Component } from 'react'
-import { Route, Switch, Redirect } from 'react-router-dom'
+import React, { Component } from 'react';
+import { Route, Switch, Redirect } from 'react-router-dom';
 
 import Container from './components/Container';
+import Login from './components/Login';
 
 class App extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {}
-  }
+	constructor(props) {
+		super(props);
+		this.state = {};
+	}
 
-  render() {
-    return (
-      <div className='App'>
-        <Switch>
-          <Route path='/' component={Container} />
-          <Redirect to='/' />
-        </Switch>
-      </div>
-    )
-  }
+	render() {
+		return (
+			<div className='App'>
+				<Switch>
+					<Route path='/login' component={Login} />
+					<Route path='/' component={Container} />
+					<Redirect to='/' />
+				</Switch>
+			</div>
+		);
+	}
 }
 
-export default App
+export default App;
