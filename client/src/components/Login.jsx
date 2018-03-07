32300f330201f5df9655ca0e65041ef77c5bc9a8
Connie <connie90503@gmail.com>
2018-03-06 17:38:35 -0800

    Committing all files, including Login.jsx

---
+++ client/src/components/Login.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import RaisedButton from 'material-ui/RaisedButton';
+
+
+class Login extends Component {
+	render() {
+		return (
+			<div>
+				<RaisedButton 
+					backgroundColor="#C960FF"
+					label = "Login"
+				> </RaisedButton>
+			</div>
+		);
+	}   
+}
+export default Login;
+
+
