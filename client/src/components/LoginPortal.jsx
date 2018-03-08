3e22ba8af2927c129f51930e0f2d27bd6ef33f61
Connie <connie90503@gmail.com>
2018-03-07 18:14:29 -0800

    Renamed Login.jsx to LoginPortal.jsx

--- client/src/components/LoginPortal.jsx
+++ client/src/components/LoginPortal.jsx
@@ -1,15 +1,20 @@
 import React, { Component } from 'react';
 import RaisedButton from 'material-ui/RaisedButton';
 
-
 class LoginPortal extends Component {
 	render() {
 		return (
 			<div>
+                <body>
 				<RaisedButton 
 					backgroundColor="#C960FF"
 					label="Login"> 
 				</RaisedButton>
+                <RaisedButton 
+					backgroundColor="#C960FF"
+					label="Sign Up"> 
+				</RaisedButton>
+                </body>
 			</div>
 		);
 	}   
