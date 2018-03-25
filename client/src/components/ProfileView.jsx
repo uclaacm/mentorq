fd3d82f133141565973a2c7353ff0e2d0e0ed90a
Connie <connie90503@gmail.com>
2018-03-24 17:20:06 -0700

    Added text fields

--- client/src/components/ProfileView.jsx
+++ client/src/components/ProfileView.jsx
@@ -1,25 +1,32 @@
 import React, { Component } from 'react';
 import {Card, CardTitle} from 'material-ui/Card';
 import RaisedButton from 'material-ui/RaisedButton';
+import TextField from 'material-ui/TextField';
 
-class ProfileView extends Component {
+class ProfileView extends Component {	
 	render() {
 		return (
 			<div>
 				<h1>Your Account</h1>
 				<Card>
-					<CardTitle title="Name"/>
-					<CardTitle title="Email"/>
-					<CardTitle title="Phone"/>
-					<CardTitle title="Skills"/>
+					<CardTitle>Name</CardTitle>
+					<TextField hintText="Name" 
+						fullWidth={true}/><br />
+					<CardTitle>Email</CardTitle>
+					<TextField hintText="Email"
+						fullWidth={true}/><br />
+					<CardTitle>Phone</CardTitle>
+					<TextField hintText="Phone"
+						fullWidth={true}/><br />
+					<CardTitle>Skills</CardTitle>
 					<RaisedButton label="Save Profile"
 						backgroundColor="#32cd32"
 						labelColor="#ffffff"/>
 				</Card>
-                
-			</div>
+			</div> 
 		);
 	}
 }
 //Use chips for skills
+
 export default ProfileView;
