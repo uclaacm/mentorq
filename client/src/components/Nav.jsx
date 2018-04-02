7bf37dad6fc0f7738723c41a75eebe2228ef5e14
Connie Chen <33800628+conniec218@users.noreply.github.com>
2018-04-02 15:49:25 -0700

    Added Login Button to NavBar (#13)
    * Added Login Button to NavBar
    
    * Moved binding of this to handleClick to constructor

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -1,32 +1,35 @@
 import React, { Component } from 'react';
 import AppBar from 'material-ui/AppBar';
+import FlatButton from 'material-ui/FlatButton';
 
 // TODO: Replace placeholder nav bar with real thing.
 class Nav extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {};
-        this.styles = {
-            title: {
-                cursor: 'pointer',
-            }
-        };
-    }
+	constructor(props) {
+		super(props);
+		this.state = {};
+		this.styles = {
+			title: {
+				cursor: 'pointer',
+			}
+		};
+		this.handleClick = this.handleClick.bind(this);
+	}
 
-    handleClick = () => {
-        console.log("Clicked");
-    }
+	handleClick() {
+		console.log('Clicked');
+	}
 
-    render() {
-        return (
-            <AppBar
-                className="Nav"
-                title={<span style={this.styles.title}>MentorQ</span>}
-                onTitleClick={this.handleClick}
-                iconClassNameRight="muidocs-icon-navigation-expand-more"
-            />
-        );
-    }
+	render() {
+		return (
+			<AppBar
+				className="Nav"
+				title={<span style={this.styles.title}>MentorQ</span>}
+				onTitleClick={this.handleClick}
+				iconClassNameRight="muidocs-icon-navigation-expand-more"
+				iconElementRight={<FlatButton label="Login" />}
+			/>
+		);
+	}
 
 }
 
