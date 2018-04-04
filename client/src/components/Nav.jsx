b4e55fabede1acd846c1b09a312183a8cd597155
Shannon Phu <shannonphu@gmail.com>
2018-04-04 16:29:46 -0700

    Added login url to button (#14)

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import AppBar from 'material-ui/AppBar';
 import FlatButton from 'material-ui/FlatButton';
 
-// TODO: Replace placeholder nav bar with real thing.
 class Nav extends Component {
 	constructor(props) {
 		super(props);
@@ -12,11 +11,6 @@ class Nav extends Component {
 				cursor: 'pointer',
 			}
 		};
-		this.handleClick = this.handleClick.bind(this);
-	}
-
-	handleClick() {
-		console.log('Clicked');
 	}
 
 	render() {
@@ -24,9 +18,8 @@ class Nav extends Component {
 			<AppBar
 				className="Nav"
 				title={<span style={this.styles.title}>MentorQ</span>}
-				onTitleClick={this.handleClick}
 				iconClassNameRight="muidocs-icon-navigation-expand-more"
-				iconElementRight={<FlatButton label="Login" />}
+				iconElementRight={<FlatButton label="Login" href="http://localhost:8080/auth/google" />}
 			/>
 		);
 	}
