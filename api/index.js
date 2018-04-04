7adae538ff16ba65d09e638796a2c21fbdc1fea2
Galen Wong <33505320+GalenWong@users.noreply.github.com>
2018-04-04 16:26:29 -0700

    Implement Google Auth with Passport.js (#12)
    * added passport library
    
    * implemented google oAuth with passport.js
    
    * fixed api/package.json comma error
    
    * created auth router and redirect back to previous path before login; dynamically load google api secret; fix CORS issues
    
    * add /user/current endpoint to validate logging in

--- api/index.js
+++ api/index.js
@@ -4,15 +4,22 @@ const express = require('express');
 const http = require('http');
 const app = express();
 const server = http.createServer(app);
+const cors = require('cors');
 const config = require('./config');
 
 // Connect to database
 require('./models');
 
+app.use(cors({
+	credentials: true,
+	origin: true
+}));
+
 // Set up router endpoints
+const authRouter = require('./routes/auth')(app);
+app.use('/auth', authRouter);
 const userRouter = require('./routes/user');
 app.use('/user', userRouter);
-require('./routes/socket')(server);
 
 server.listen(config.server.port, () => {
 	console.log('Listening on port ' + config.server.port);
