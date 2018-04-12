1dc985c04c1bf08d4cd46c3e618ddec731088c7a
dustinnewman98 <dustinnewman98@gmail.com>
2018-04-11 18:28:19 -0700

    Ticket.js

---
+++ api/models/Ticket.js
@@ -0,0 +1,71 @@
+'use strict';
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+var Ticket;
+
+var ticketSchema = Schema({
+	requestor: {type: Schema.type.ObjectId, ref: 'User', required: true},
+	mentor: {type: Schema.type.ObjectId, ref: 'User'},
+	timeFiled: {type: Date, required: true},
+	problem: {type: String, required: true},
+	comments: String,
+	tableNum: {type: String, required: true},
+	isActive: {type: Boolean, default: true},
+	isResolved: {type: Boolean, default: false}
+});
+
+ticketSchema.statics.create = function(requestor, problem, tableNum) {
+	var ticket = new this({
+		requestor: requestor,
+		timeFiled: Date.now,
+		problem: problem,
+		tableNum: tableNum
+	});
+    
+	return new Promise((resolve, reject) => {
+		ticket.save((error, newTicket) => {
+			if (error) reject(error);
+			else resolve(newTicket);
+		});
+	});
+};
+
+ticketSchema.statics.addComments = function(comments) {
+	this.comments = comments;
+
+	return new Promise((resolve, reject) => {
+		this.save((error, ticket) => {
+			if (error) reject(error);
+			else resolve(ticket);
+		});
+	});
+};
+
+ticketSchema.statics.claim = function(mentor) {
+	this.mentor = mentor;
+	this.isActive = false;
+
+	return new Promise((resolve, reject) => {
+		this.save((error, ticket) => {
+			if (error) reject(error);
+			else resolve(ticket);
+		});
+	});
+};
+
+ticketSchema.statics.resolve = function() {
+	this.isResolved = true;
+
+	return new Promise((resolve, reject) => {
+		this.save((error, ticket) => {
+			if (error) reject(error);
+			else resolve(ticket);
+		});
+	});
+};
+
+Ticket = mongoose.model('Ticker', ticketSchema);
+
+module.exports = Ticket;
