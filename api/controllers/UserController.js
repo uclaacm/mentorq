c14616a6ed086854b3a92f4ea948e5f3534ae4ad
GalenWong <wonggalen1999@gmail.com>
2018-04-13 22:43:50 -0700

    activeMentors function finished

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -25,26 +25,17 @@ function activeMentors(req, res) {
 
 	for (const socketID in connectedSockets) {
 		const socket = connectedSockets[socketID];
-		const { client } = socket; // https://socket.io/docs/server-api/#socket-client
-		const req = client.request; // https://socket.io/docs/server-api/#client-request
+		const { client } = socket;
+		const req = client.request;
+		const passport = req.session.passport;
 
-		console.log(req);
-		// Now you can access the req.session object you were looking for
-		// in the first place.
-	}
-	/*		Galen's old method
-	var sess = req.sessionStore.sessions;
-
-	for(var connection in sess){
-		console.log(sess[connection]);
-		var connectionSess = JSON.parse(sess[connection]);
-		var userObj = connectionSess['passport'];
-
-		if (userObj && userObj['user']['isMentor']) {	// if user is not logged in, won't check info
-			activeMentors.push(userObj['user']);
+		if (passport) {
+			const user = passport.user;
+			if (user.isMentor) {
+				activeMentors.push(user);
+			}
 		}
 	}
-	*/
 	res.json(activeMentors);
 }
 
