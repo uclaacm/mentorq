9c6ae97d919aeeea380447f6893536248edcecce
GalenWong <wonggalen1999@gmail.com>
2018-04-18 18:58:17 -0700

    TicketList completed

---
+++ client/src/components/TicketList.jsx
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import Ticket from './Ticket';
+
+class TicketList extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			tickets: [],
+		};
+		this.state.tickets.push(<Ticket />);
+		this.state.tickets.push(<Ticket />);
+		this.state.tickets.push(<Ticket />);
+		this.state.tickets.push(<Ticket />);
+	}
+
+	render() {
+		return (
+			<div className="ticketList">
+				{this.state.tickets}
+			</div>
+		);
+	}
+}
+
+export default TicketList;
