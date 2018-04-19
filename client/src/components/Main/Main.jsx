90038c9176c580555329d4502b941c4e5bb009b1
shannonphu <shannonphu@sbcglobal.net>
2018-04-18 21:31:02 -0700

    remove unneeded constructor

--- client/src/components/Main/Main.jsx
+++ client/src/components/Main/Main.jsx
@@ -3,10 +3,6 @@ import { Nav } from '../';
 import { Route, Switch } from 'react-router-dom';
 
 class Main extends Component {
-	constructor(props) {
-		super(props);
-	}
-
 	render() {
 		return (
 			<div>
