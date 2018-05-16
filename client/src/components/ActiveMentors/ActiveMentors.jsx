ba0897875b685ad09b19722a6ea615d96ab324db
Shannon Phu <shannonphu@gmail.com>
2018-05-15 21:57:00 -0700

    Fixed compilation error with ActiveMentor after material-ui upgrade (#41)

--- client/src/components/ActiveMentors/ActiveMentors.jsx
+++ client/src/components/ActiveMentors/ActiveMentors.jsx
@@ -1,33 +1,41 @@
 import React, { Component } from 'react';
 import Card, { CardContent } from 'material-ui/Card';
 import { Lens } from '@material-ui/icons';
+import PropTypes from 'prop-types';
 
 const iconStyles = {
 	fontSize: 16,
 	color: '#4CAF50',
 	display: 'inline-flex',
-	verticalAlign: 'top',
+	verticalAlign: 'top'
 };
 
 class ActiveMentors extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			waitMinute: 100,
+			waitMinute: 100
 		};
 		this.props.getActiveMentors();
 	}
 
+	static get propTypes() {
+		return {
+			user: PropTypes.object.isRequired,
+			getActiveMentors: PropTypes.func.isRequired
+		};
+	}
+
 	render() {
 		const numActive = this.props.user.mentors.length;
-		return(
+		return (
 			<Card>
 				<CardContent>
 					<p>
-						<FontIcon className='material-icons' style={iconStyles}>lens</FontIcon>
-						<strong>{` `}{numActive}</strong>
-						{`${numActive > 1 ?' mentors':' mentor'}
-						online. Estimated wait: `} <strong>{this.state.waitMinute} minutes</strong>
+						<Lens style={iconStyles} />
+						<strong>{` ${numActive}`}</strong>
+						{`${numActive > 1 ? ' mentors' : ' mentor'} online. 
+						Estimated wait: `} <strong>{this.state.waitMinute} minutes</strong>
 					</p>
 				</CardContent>
 			</Card>
