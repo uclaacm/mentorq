d2c4cb07e1d6c8648231e1f7ab96f3a89e80a31c
Shannon Phu <shannonphu@gmail.com>
2018-05-30 21:47:34 -0700

    Change proptype type checking to property assignment (#70)
    * Resolves #61 Change proptype type checking to property assignment
    
    * Update prop naming to be consistent with TicketForm/backend

--- client/src/components/TicketForm/TicketForm.jsx
+++ client/src/components/TicketForm/TicketForm.jsx
@@ -14,7 +14,6 @@ import './TicketForm.css';
 class TicketForm extends Component {
 	constructor(props) {
 		super(props);
-		console.log(props);
 		this.state = {
 			description: {
 				value: '',
@@ -31,13 +30,6 @@ class TicketForm extends Component {
 		};
 		this.onSubmit = this.onSubmit.bind(this);
 		this.handleChange = this.handleChange.bind(this);
-	}
-
-	static get propTypes() {
-		return {
-			submitTicket: PropTypes.func.isRequired,
-			socket: PropTypes.object.isRequired
-		};
 	}
 
 	handleChange(e) {
@@ -168,5 +160,10 @@ class TicketForm extends Component {
 		);
 	}
 }
+
+TicketForm.propTypes = {
+	submitTicket: PropTypes.func.isRequired,
+	socket: PropTypes.object.isRequired
+};
 
 export default TicketForm;
