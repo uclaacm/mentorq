2abcd5cc2b8a191c9e61abd3e6a566fcab1feca1
GalenWong <wonggalen1999@gmail.com>
2018-11-11 10:53:06 -0800

    UPT: remove imports in nav

--- client/src/components/Nav/Nav.jsx
+++ client/src/components/Nav/Nav.jsx
@@ -3,8 +3,6 @@ import AppBar from '@material-ui/core/AppBar';
 import Button from '@material-ui/core/Button';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import IconButton from '@material-ui/core/IconButton';
-import AccountCircle from '@material-ui/icons/AccountCircle';
 import PropTypes from 'prop-types';
 
 import { Link } from 'react-router-dom';
