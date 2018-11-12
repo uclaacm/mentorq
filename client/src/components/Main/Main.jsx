a251d5a1262514406b1e12ee0fe05a02d5d83d9a
Timothy Gu <timothygu99@gmail.com>
2018-11-11 16:35:31 -0800

    Add MuiThemeProvider with purple (#96)

--- client/src/components/Main/Main.jsx
+++ client/src/components/Main/Main.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Route, Switch, Redirect } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import purple from '@material-ui/core/colors/purple';
 
 import {
 	Nav,
@@ -10,29 +12,37 @@ import {
 	AdminPanel
 } from '../';
 
+const theme = createMuiTheme({
+	palette: {
+		primary: purple
+	}
+});
+
 function Main({ isSignedIn, isAdmin }) {
 	return (
-		<div>
-			<Nav isSignedIn={isSignedIn} isAdmin={isAdmin} />
+		<MuiThemeProvider theme={theme}>
+			<div>
+				<Nav isSignedIn={isSignedIn} isAdmin={isAdmin} />
 
-			{/* Alternate pages beneath navbar, based on current route*/}
-			<Switch>
-				<Route exact path='/' render={() => <HomeContainer isSignedIn={isSignedIn} />} />
+				{/* Alternate pages beneath navbar, based on current route*/}
+				<Switch>
+					<Route exact path='/' render={() => <HomeContainer isSignedIn={isSignedIn} />} />
 
-				{/* <Route path='/profile' render={() =>
-					isSignedIn ? <ConnectedProfileView /> : <Redirect to='/' />
-				} /> */}
-				<Route path='/tickets' render={() =>
-					isSignedIn ? <ConnectedTicketList /> : <Redirect to='/' />
-				} />
-				<Route path='/mentors' render={() =>
-					isSignedIn ? <ConnectedActiveMentors /> : <Redirect to='/' />
-				} />
-				<Route path='/admin' render={() =>
-					isAdmin ? <AdminPanel /> : <Redirect to='/' />
-				} />
-			</Switch>
-		</div>
+					{/* <Route path='/profile' render={() =>
+						isSignedIn ? <ConnectedProfileView /> : <Redirect to='/' />
+					} /> */}
+					<Route path='/tickets' render={() =>
+						isSignedIn ? <ConnectedTicketList /> : <Redirect to='/' />
+					} />
+					<Route path='/mentors' render={() =>
+						isSignedIn ? <ConnectedActiveMentors /> : <Redirect to='/' />
+					} />
+					<Route path='/admin' render={() =>
+						isAdmin ? <AdminPanel /> : <Redirect to='/' />
+					} />
+				</Switch>
+			</div>
+		</MuiThemeProvider>
 	);
 }
 
