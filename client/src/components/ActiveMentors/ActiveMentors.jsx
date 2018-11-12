ea94b65c9723175e0a5cff29810420f886273e1e
GalenWong <wonggalen1999@gmail.com>
2018-11-11 19:14:31 -0800

    UPT: use new styling with activementors

--- client/src/components/ActiveMentors/ActiveMentors.jsx
+++ client/src/components/ActiveMentors/ActiveMentors.jsx
@@ -1,23 +1,30 @@
 import React from 'react';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+import { withStyles } from '@material-ui/core/styles';
 import { Lens } from '@material-ui/icons';
 import PropTypes from 'prop-types';
 
 import { userShape } from '../../shapes';
 
-const iconStyles = {
-	fontSize: 16,
-	color: '#4CAF50',
-	display: 'inline-flex',
-	verticalAlign: 'top'
-};
+const styles = () => ({
+	icon: {
+		color: '#4CAF50',
+		display: 'inline-flex',
+		verticalAlign: 'top'
+	},
+	text: {
+		fontSize: 16
+	}
+});
 
 let initing = true;
 
 function ActiveMentors({
 	mentors,
-	getActiveMentors
+	getActiveMentors,
+	classes
 }) {
 	// TODO: Make this live.
 	if (initing) {
@@ -29,12 +36,12 @@ function ActiveMentors({
 	return (
 		<Card>
 			<CardContent>
-				<p>
-					<Lens style={iconStyles} />
-					<strong>{` ${numActive}`}</strong>
+				<Typography component='p' className={classes.text}>
+					<Lens className={classes.icon} />{' '}
+					<strong>{numActive}</strong>
 					{`${numActive > 1 ? ' mentors' : ' mentor'} online.`}
 					{/* Estimated wait: <strong>{waitMinute} minutes</strong> */}
-				</p>
+				</Typography>
 			</CardContent>
 		</Card>
 	);
@@ -42,11 +49,12 @@ function ActiveMentors({
 
 ActiveMentors.propTypes = {
 	mentors: PropTypes.arrayOf(PropTypes.shape(userShape).isRequired),
-	getActiveMentors: PropTypes.func.isRequired
+	getActiveMentors: PropTypes.func.isRequired,
+	classes: PropTypes.object.isRequired
 };
 
 ActiveMentors.defaultProps = {
 	mentors: []
 };
 
-export default ActiveMentors;
+export default withStyles(styles)(ActiveMentors);
