cce8b41ac9f76a52cec4ddb2cdf8751ca33f7210
Timothy Gu <timothygu99@gmail.com>
2018-11-15 23:46:18 -0800

    Add production config

--- client/src/config/index.js
+++ client/src/config/index.js
@@ -1,6 +1,15 @@
 export const server = {};
 
 switch (process.env.NODE_ENV) {
+case 'production':
+default:
+	// Server settings
+	server.scheme = 'https';
+	server.host = 'api.mentorq.hack.uclaacm.com';
+	server.port = 443;
+	server.path = '';
+	break;
+
 case 'development':
 default:
 	// Server settings
