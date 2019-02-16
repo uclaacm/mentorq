4ecf845d13d1f4cdb83fed174122a70d040db04a
Timothy Gu <timothygu99@gmail.com>
2019-02-15 20:28:20 -0800

    Add build

---
+++ prod/build/precache-manifest.80e70afcd420b7de04ba6ad8c48e2f98.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9949df25e6c3f86efd46",
+    "url": "/static/css/main.8af1b554.chunk.css"
+  },
+  {
+    "revision": "9949df25e6c3f86efd46",
+    "url": "/static/js/main.9949df25.chunk.js"
+  },
+  {
+    "revision": "4c5790614e9dfdb95353",
+    "url": "/static/js/1.4c579061.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "624fab3f177d3b1c3e902747f0f6ff80",
+    "url": "/index.html"
+  }
+];
