26f5ba84cd542662bc5242f919b25ab46253f579
Timothy Gu <timothygu99@gmail.com>
2019-04-13 18:41:12 -0700

    Add create

--- api/models-postgres/User.js
+++ api/models-postgres/User.js
@@ -3,7 +3,27 @@
 const Sequelize = require('sequelize');
 const { sequelize } = require('./index.js');
 
-const User = sequelize.define('user', {
+class User extends Sequelize.Model {
+	async create(name, email, phone, googleId) {
+		return super.create({ name, email, googleId });
+	}
+
+	/**
+	 * Read and Retrieve a User object from the database
+	 * @param {string} user's googleID (as hash)
+	 * @returns {User} one User object with matching ID
+	 * @example
+	 * const user = await User.read('someGoogleIdHash');
+	 * console.log(user);
+	 */
+	async read(googleId) {
+		return this.findOne({ where: { googleId } });
+	}
+}
+User.getById = User.findById;
+User.getAll = User.all;
+
+User.init({
 	name: {
 		type: Sequelize.STRING,
 		allowNull: false,
@@ -18,24 +38,16 @@ const User = sequelize.define('user', {
 		unique: true,
 		allowNull: false,
 	},
-});
-
-Object.assign(User, {
-	/**
-	 * Read and Retrieve a User object from the database
-	 * @param {string} user's googleID (as hash)
-	 * @returns {User} one User object with matching ID
-	 * @example
-	 * const user = await User.read('someGoogleIdHash');
-	 * console.log(user);
-	 */
-	async read(googleId) {
-		return this.findOne({ where: { googleId } });
+	isMentor: {
+		type: Sequelize.BOOLEAN,
+		allowNull: false,
+		defaultValue: false,
 	},
-
-	getById: User.findById,
-
-	getAll: User.all
-});
+	isAdmin: {
+		type: Sequelize.BOOLEAN,
+		allowNull: false,
+		defaultValue: false,
+	},
+}, { sequelize });
 
 module.exports = User;
