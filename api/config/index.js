cd35cbb3909dad00804d9f94cc6cd28c55a7e42d
Timothy Gu <timothygu99@gmail.com>
2019-05-25 17:06:00 -0700

    Remove stale config entries

--- api/config/index.js
+++ api/config/index.js
@@ -4,32 +4,18 @@ module.exports = (() => {
 	switch (process.env.NODE_ENV) {
 	case 'production':
 		return {
-			// MongoDB connection settings
-			database: {
-				uri: 'mongodb://db_mongo'
-			},
-
 			// Server settings
 			serverBaseURL: 'https://api.mentorq.hack.uclaacm.com/',
 			serverPort: 8080,
-			frontendBaseURL: 'https://mentorq.hack.uclaacm.com/',
-
-			enablePostgres: false
+			frontendBaseURL: 'https://mentorq.hack.uclaacm.com/'
 		};
 	case 'development':
 	default:
 		return {
-			// MongoDB connection settings
-			database: {
-				uri: 'mongodb://db_mongo'
-			},
-
 			// Server settings
 			serverBaseURL: 'http://localhost:8080/',
 			serverPort: 8080,
-			frontendBaseURL: 'http://localhost:3000/',
-
-			enablePostgres: true
+			frontendBaseURL: 'http://localhost:3000/'
 		};
 	}
 })();
